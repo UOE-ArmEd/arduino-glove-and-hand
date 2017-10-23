03226f9e4a4e1f552f17d275350eb62a216ae119
Kyle Montemayor <kmonte@umd.edu>
2017-10-23 21:49:49 +0100

    better comments

--- filter.h
+++ filter.h
@@ -5,23 +5,42 @@
  *
  * If we wanted to, it would be pretty easy to weight the filter
  * 
+ * TODO: Per buffer lengths
+ * TODO: Make sure it works with arduino (Might have to make it a class?)
+ * TODO: Make sure we can instantiate multiple buffers (malloc maybe, or with classes idk)
+ * TODO: Weighted rolling average
+ * TODO: Properly test __EVERYTHING__
+ *
  * Kyle Montemayor
  * kmonte@umd.edu
  */
 #ifndef __FILTER_H__
 #define __FILTER_H__
 
-#define WINDOW_LENGTH 16
 /*
- * Returns the average of the buffer
+ * How large the buffer is
+ * Needs to be tweaked
+ */
+#define WINDOW_LENGTH 16
+
+/*
+ * Computes and returns the average in the buffer
+ * I think it's better to computer it here vs in 
+ * get_average() because we should be updating the
+ * buffer more than we poll it
+ *
+ * If not then we can computer the average as we add new values
+ *
  */
 float get_average();
 
 /*
  * Adds a new value to the buffer
- * Currently computes the average here, 
- * though I think it's weird for this to
- * return anything other than a success
+ * Will overwrite old values
+ * really not exciting, maybe have
+ * it return the total number of
+ * items that have ever been put in
+ * the buffer
  */
 void add_value(short);
 
